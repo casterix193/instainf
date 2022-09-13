a3bd2c6f9f9a4bf10ad6e3e624ff99d15e7dc95b
Dharmil Desai <42259391+casterix193@users.noreply.github.com>
2022-09-13 12:16:12 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+setup(
+    name='instainf',
+    version="1.26",
+    packages=find_packages(),
+    author="casterix",
+    install_requires=["argparse","requests","phonenumbers","pycountry"],
+    description="A tool to get all probable informtation from a user's instagram account",
+    long_description="It is a tool written to retrieve private information such as User ID, Bio, Phone Number, Mail Address from Instagram accounts via API.",
+    include_package_data=True,
+    url='http://github.com/casterix193',
+    entry_points = {'console_scripts': ['instainf = instainf.core:main']},
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
